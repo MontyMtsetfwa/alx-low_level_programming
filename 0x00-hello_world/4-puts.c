824c4fcc3a691cd12ee67d57c97499b90f9c47a0
MontyMtsetfwa <montymtsetfwa@gmail.com>
2022-12-08 09:50:36 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,13 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * *  * main - Entry point   
+ * Return:o      */
 
 int main(void)
 
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
 
 }
